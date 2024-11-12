f4a9e1ac931a4bf26b6a8579bb89e5ecaf1c3d8b
kartik7974 <Kartik@1403>
2024-11-12 17:32:56 +0530

    Prepare for deployment

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_23_123411) do
-
+ActiveRecord::Schema[7.2].define(version: 2024_10_23_123411) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,8 +18,7 @@ ActiveRecord::Schema.define(version: 2024_10_23_123411) do
     t.string "email"
     t.string "password"
     t.string "full_name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
-
 end
